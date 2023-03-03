380c0466c0c88f0a0d3267eae2182961af476046
Sharif.T <Shariftarver@gmail.com>
2023-03-03 01:02:19 -0500

    rust implentation climbstairs w/ test cases

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,20 @@ fn main() {
     let n = 2;
     println!("{}", climb_stairs(n));
 }
+
+// Test cases
+
+#[test]
+fn test_climb_stairs() {
+    assert_eq!(climb_stairs(2), 2);
+    assert_eq!(climb_stairs(3), 3);
+    assert_eq!(climb_stairs(4), 5);
+    assert_eq!(climb_stairs(5), 8);
+    assert_eq!(climb_stairs(6), 13);
+    assert_eq!(climb_stairs(7), 21);
+}
+
+#[test]
+fn test_climb_stairs_one() {
+    assert_eq!(climb_stairs(1), 1);
+}
---
+++ target/debug/deps/rust-5aa244c32e18b1c7.d
@@ -0,0 +1,5 @@
+/Users/shariftarver/Desktop/software_engineering/mastering-rust/algorithms-rust/src/yeetcode/climbStairs/rust/target/debug/deps/rust-5aa244c32e18b1c7: src/main.rs
+
+/Users/shariftarver/Desktop/software_engineering/mastering-rust/algorithms-rust/src/yeetcode/climbStairs/rust/target/debug/deps/rust-5aa244c32e18b1c7.d: src/main.rs
+
+src/main.rs:
